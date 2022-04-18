3700aa64117fb95dd920e67de74c02b987a46966
Nathan <frost.nathan1995@gmail.com>
2022-04-17 22:01:47 -0400

    Making Git Dir

--- ES6 Practice.js
+++
@@ -1,7 +0,0 @@
-const Pi = 3.14;
-
-//var cannot be redeclared using let and let is block scoped.
-
-//const is constant, cannot be redeclared or reassigned.
-
-//hoisting is lifting value to the top of the scope that they are declared on. ie. var can be accessed before it is used later while let is confined to the unit being worked on in question.
